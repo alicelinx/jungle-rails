e2dd41a81988b033ac8d8873f7dc455e52bfe160
Alice Lin <alicexxlin@gmail.com>
2023-07-27 03:17:19 +0000

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  
+  get '/about' => 'about#index', as: :about
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
